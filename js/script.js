5f6429a10b4e92b3cbf98ed91d4ea1cfc9be4acc
Norbert Mielczarek <nri1234.nm@gmail.com>
2019-05-27 15:51:58 +0200

    Add content to Carousel

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity(elem, {
+    cellAlign: 'left',
+    contain: true,
+    hash: true,
+});
+
+var progressBar = document.querySelector('.progress-bar');
+var btnStart = document.querySelector('.btn');
+
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+});
+
+
+var flkty = new Flickity('.main-carousel', {
+
+});
+
+
+
+
+btnStart.addEventListener('click', function (event) {
+
+    if (!matchesSelector(event.target, '.btn')) {
+        return;
+    }
+    var selector = event.target.getAttribute('data-selector');
+    flkty.selectCell(selector);
+});
