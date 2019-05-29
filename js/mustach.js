14a12ed8baab112b26f317824678f7190d6f692c
Norbert Mielczarek <nri1234.nm@gmail.com>
2019-05-29 17:17:30 +0200

    Modified content of Carousel

--- js/mustach.js
+++ js/mustach.js
@@ -2,16 +2,15 @@
 (function () {
 
     var carouselItem = document.getElementById('carousel-item').innerHTML;
+
     Mustache.parse(carouselItem);
 
 
     var listItems = '';
 
     for (var i = 0; i < slidesData.length; i++) {
-        console.log(slidesData);
         listItems += Mustache.render(carouselItem, slidesData[i]);
-    }
-
+    };
     results.insertAdjacentHTML('beforeend', listItems);
 
 })();
