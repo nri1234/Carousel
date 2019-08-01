f00f1b58e64f25f19fe0c207f32c9b00403e45e9
Norbert Mielczarek <nri1234.nm@gmail.com>
2019-08-01 16:42:46 +0200

    Made changes to project

---
+++ js/mustach.js
@@ -0,0 +1,16 @@
+'use strict';
+(function () {
+
+    var carouselItem = document.getElementById('carousel-item').innerHTML;
+
+    Mustache.parse(carouselItem);
+
+
+    var listItems = '';
+
+    for (var i = 0; i < slidesData.length; i++) {
+        listItems += Mustache.render(carouselItem, slidesData[i]);
+    };
+    results.insertAdjacentHTML('beforeend', listItems);
+
+})();
